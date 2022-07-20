0678c19cb8063da493855b0d3a3bdc5484843941
lead1974 <email2lead@gmail.com>
2022-07-20 07:09:37 -0700

    sign-in-google

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,20 +1,23 @@
 import {
-    signInWithGooglePopup,
-    createUserProfileDocument,
-  } from '../../utils/firebase/firebase.utils';
-  
-  const SignIn = () => {
-    const logGoogleUser = async () => {
-      const response = await signInWithGooglePopup();
-      createUserProfileDocument(response);
-    };
-  
-    return (
-      <div>
-        <h1>Sign In Page</h1>
-        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      </div>
-    );
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
-  
-  export default SignIn;
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      {/* <SignUpForm /> */}
+    </div>
+  );
+};
+
+export default SignIn;
